0c46ebaefbcd7a1a45e229502540df4d5fcde1df
Alfian Taka <pianaee379@gmail.com>
2026-02-19 21:23:06 +0700

    bronze: Shell Game and its comments

--- Bronze/simulation/Shell Game/shell-game.cpp
+++ Bronze/simulation/Shell Game/shell-game.cpp
@@ -11,15 +11,17 @@ int main() {
   scanf("%d", &n);
 
   vector<int> shell_at_pos(3);
-  for (int i = 0; i < 3; i++) { shell_at_pos[i] = i; }
+  for (int i = 0; i < 3; i++) { shell_at_pos[i] = i; } // 0 1 2
 
   vector<int> counter(3);
   for (int i = 0; i < n; i++) {
-    int a, b, g;
+    int a, b, g; // 121, 321, 131
     scanf("%d %d %d", &a, &b, &g);
-    a--, b--, g--;
+    a--, b--, g--; // 010, 210, 020
 
-    std::swap(shell_at_pos[a], shell_at_pos[b]);
+    std::swap(shell_at_pos[a], shell_at_pos[b]); // 102, 120, 021
+    // 0, 0, 0
+    // 1, 2, 0
     counter[shell_at_pos[g]]++;
   }
 
