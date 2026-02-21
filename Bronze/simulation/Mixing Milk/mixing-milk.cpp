f7ff9c0400f2b8483f905cbf93f9066b3f8dec14
Alfian Taka <pianaee379@gmail.com>
2026-02-22 06:54:16 +0700

    bronze: Mixing Milk and its comments

--- Bronze/simulation/Mixing Milk/mixing-milk.cpp
+++ Bronze/simulation/Mixing Milk/mixing-milk.cpp
@@ -16,10 +16,13 @@ int main() {
   for (int i = 0; i < N; i++) { scanf("%d %d", &capacity[i], &milk[i]); }
 
   for (int i = 0; i < TURN_NUM; i++) {
-    int bucket1 = i % N;
-    int bucket2 = (i + 1) % N;
+    int bucket1 = i % N; // 0, 1, 2, ...
+    int bucket2 = (i + 1) % N; // 1, 2, 0, ...
 
-    int amt = min(milk[bucket1], capacity[bucket2] - milk[bucket2]);
+    // milk[bucket1] = 3, 4, 5, ...
+    // capacity[bucket2] = 11, 12, 10, ...
+    // milk[bucket2] = 4, 5, 3, ...
+    int amt = min(milk[bucket1], capacity[bucket2] - milk[bucket2]); // 12, 10
 
     milk[bucket1] -= amt;
     milk[bucket2] += amt;
